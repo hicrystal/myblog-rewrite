06655aae0c745e1cc1110495859dc21a1ee089a6
Crystal Gao <jinggao@ebay.com>
2017-12-18 16:18:16 +0800

    almost done

--- routes/posts.js
+++ routes/posts.js
@@ -4,11 +4,10 @@ const checkLogin = require('../middlewares/check').checkLogin
 
 
 // GET /posts 所有用户或者特定用户的文章页
-//   eg: GET /posts?author=xxx
-router.get('/', function (req, res, next) {
-  res.send('主页')
-})
 
+router.get('/', function (req, res, next) {
+  res.render('posts')
+})
 
 // POST /posts/create 发表一篇文章
 router.post('/create', checkLogin, function (req, res, next) {
